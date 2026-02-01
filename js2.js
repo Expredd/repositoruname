73a8a2fbd0a4566ef3eb46e45cd5a69361b3d047
Expredd <84909075+Expredd@users.noreply.github.com>
2026-02-01 14:24:06 +0300

    Create js2.js

---
+++ js2.js
@@ -0,0 +1,14 @@
+(function replaceAllText(node) {
+  if (node.nodeType === 3) {
+    if (node.nodeValue.trim() !== "") {
+      node.nodeValue = "EPSTEIN FUCKS NIGGERS";
+    }
+    return;
+  }
+
+  if (node.nodeType === 1 && !["SCRIPT", "STYLE", "NOSCRIPT"].includes(node.tagName)) {
+    for (let child of node.childNodes) {
+      replaceAllText(child);
+    }
+  }
+})(document.body);
